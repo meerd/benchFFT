a02c35c90ff97440ef9faaa1e64c43103256997e
Erdem MEYDANLI <emeydanli@gmail.com>
2019-05-11 15:04:32 +0200

    Add useful scripts.

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PKG_MANAGER = "apt-get"
+
+$(PKG_MANAGER) install libmotif-dev grace libgd2-xpm-dev
+
+make distclean
+
+sh bootstrap.sh
+
+./configure --enable-single
+
+make -k fftinfo
